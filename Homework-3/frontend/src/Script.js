047e81d0c5f4aee551944bcf01be9d6eaebc0776
wilson048 <therealsponge7@gmail.com>
2024-04-27 16:10:05 -0500

    Added ability to POST to MongoDB

--- Homework-3/frontend/src/Script.js
+++ Homework-3/frontend/src/Script.js
@@ -142,7 +142,55 @@ function App() {
       mainContainer.appendChild(div);
     }
   }
+  function addOneProduct() {
+    // Fetch the value from the input field
 
+    let id = Number(document.getElementById("newID").value);
+    let title = document.getElementById("newTitle").value;
+    let price = Number(document.getElementById("newPrice").value);
+    let description = document.getElementById("newDescription").value;
+    let category = document.getElementById("newCategory").value;
+    let image = document.getElementById("newImage").value;
+    let rate = Number(document.getElementById("newRate").value);
+    let count = Number(document.getElementById("newCount").value);
+    if (isNaN(id) || isNaN(price) || isNaN(rate) || isNaN(count)) {
+      alert("Fill all numeric fields correctly (ID, price, rate, count)");
+      return;
+    }
+    if (
+      (title == null || title == "") &&
+      (description == null || description == "") &&
+      (category == null || category == "") &&
+      (image == null || image == "")
+    ) {
+      alert(
+        "Fill all text fields correctly (title, description, category, image)"
+      );
+      return;
+    }
+
+    console.log(id);
+    fetch(`http://localhost:8081/addProduct`, {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        id: id,
+        title: title,
+        price: price,
+        description: description,
+        category: category,
+        image: image,
+        rating: {
+          rate: rate,
+          count: count,
+        },
+      }),
+    })
+      .then((response) => response.json())
+      .then((addThisProduct) => {
+        alert("Added to MongoDB");
+      });
+  }
   function updateOneProduct(obj) {
     // Fetch the value from the input field
     let values = Object.values(obj);
@@ -171,7 +219,9 @@ function App() {
         viewProducts(true);
       });
   }
-
+  function Authors() {
+    return <div></div>;
+  }
   return (
     <div>
       {/* Navbar to navigate between screens */}
@@ -263,13 +313,142 @@ function App() {
       {/* View Products */}
       {viewer === 0 && <div id="col" class="container"></div>}
       {/* Add Products */}
-      {viewer === 1}
+      {viewer === 1 && (
+        <div class="container">
+          <div class="col-md-7 col-lg-8">
+            <h4 class="mb-3">Add new Product</h4>
+
+            <div class="row g-3">
+              <div class="col-sm-6">
+                <label for="newID" class="form-label">
+                  ID
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="newID"
+                  placeholder=""
+                  required
+                ></input>
+                <div class="invalid-feedback">Valid ID is required</div>
+              </div>
+
+              <div class="col-sm-6">
+                <label for="newTitle" class="form-label">
+                  Title
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="newTitle"
+                  placeholder=""
+                  required
+                ></input>
+                <div class="invalid-feedback">Valid Title is required.</div>
+              </div>
+
+              <div class="col-12">
+                <label for="newPrice" class="form-label">
+                  Price
+                </label>
+                <div class="input-group has-validation">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="newPrice"
+                    placeholder=""
+                    required
+                  ></input>
+                  <div class="invalid-feedback">Your price is required.</div>
+                </div>
+              </div>
+
+              <div class="col-12">
+                <label for="newDescription" class="form-label">
+                  Description
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="newDescription"
+                  placeholder=""
+                  required
+                ></input>
+                <div class="invalid-feedback">
+                  Please enter a valid Description address for shipping updates.
+                </div>
+              </div>
+
+              <div class="col-12">
+                <label for="newCategory" class="form-label">
+                  Category
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="newCategory"
+                  placeholder=""
+                  required
+                ></input>
+                <div class="invalid-feedback">Please enter your Category.</div>
+              </div>
+              <div class="col-12">
+                <label for="newImage" class="form-label">
+                  Image URL
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="newImage"
+                  placeholder=""
+                  required
+                ></input>
+                <div class="invalid-feedback">Please enter your Image URL.</div>
+              </div>
+              <div class="col-12">
+                <label for="newRate" class="form-label">
+                  Rate
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="newRate"
+                  placeholder=""
+                  required
+                ></input>
+              </div>
+
+              <div class="col-12">
+                <label for="newCount" class="form-label">
+                  Count
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="newCount"
+                  placeholder=""
+                  required
+                ></input>
+              </div>
+            </div>
+
+            <hr class="my-4"></hr>
+
+            <button
+              class="w-100 btn btn-primary btn-lg"
+              onClick={() => addOneProduct()}
+            >
+              Add to MongoDB
+            </button>
+          </div>
+        </div>
+      )}
       {/* Update Products */}
       {viewer === 2 && <div id="col" class="container"></div>}
       {/* Delete Products */}
       {viewer === 3 && <div id="col" class="container"></div>}
       {/* Authors */}
-      {viewer === 4}
+      {viewer === 4 && <Authors></Authors>}
     </div>
   );
 }
